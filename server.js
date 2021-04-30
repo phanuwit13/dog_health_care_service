0de28b5ecdf26c72cb3b387ebfe6264fad058224
phanuwit <panuwit.kit@chomchob.com>
2021-04-30 12:54:06 +0700

    connect database4

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const cors = require('cors')
 const formData = require('express-form-data')
 const PORT = process.env.PORT || 8080
 
-// app.use(formData.parse())
+app.use(formData.parse())
 
-// app.use(cors())
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello World service')
 })
@@ -21,6 +21,6 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
 
-// app.listen(3000, () => {
-//   console.log('Start server at port 3000.')
-// })
+app.listen(3000, () => {
+  console.log('Start server at port 3000.')
+})
--- util/db.js
+++ util/db.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: 'wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.co',
+  host: 'wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user: 'wbj4x2s8caz4aouq',
   password: 'sxxd8dompo4bz4t8',
   database: 'lfl2n7c3v5tz3hur',
