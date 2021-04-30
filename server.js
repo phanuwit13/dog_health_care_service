fe7bbff14620e11309fa471eddf33ac54b53d260
phanuwit <panuwit.kit@chomchob.com>
2021-04-30 11:37:27 +0700

    api v1

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const app = express()
-const api = require('./routes/api');
+const api = require('./routes/api')
 var bodyParser = require('body-parser')
-const cors = require("cors");
-const formData = require("express-form-data");
-app.use(formData.parse());
+const cors = require('cors')
+const formData = require('express-form-data')
+app.use(formData.parse())
 
-
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello World big')
 })
-app.use('/api', api);
 
-app.listen(3000, () => {
-  console.log('Start server at port 3000.')
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
 })
+
+app.use('/api', api)
+
+// app.listen(3000, () => {
+//   console.log('Start server at port 3000.')
+// })
